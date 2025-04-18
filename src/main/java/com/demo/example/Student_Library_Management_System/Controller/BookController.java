7a7b77d61060e66a38867de6a20740b60cc3b68f
Mohd-Yaseen <mohdyaseen2856@gmail.com>
2025-04-18 15:45:09 +0530

    Add some controller and service classes

---
+++ src/main/java/com/demo/example/Student_Library_Management_System/Controller/BookController.java
@@ -0,0 +1,4 @@
+package com.demo.example.Student_Library_Management_System.Controller;
+
+public class BookController {
+}
---
+++ src/main/java/com/demo/example/Student_Library_Management_System/Service/CardService.java
@@ -0,0 +1,7 @@
+package com.demo.example.Student_Library_Management_System.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CardService {
+}
